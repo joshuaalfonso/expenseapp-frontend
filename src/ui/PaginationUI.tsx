50182e7966f8658dbf2548f322379fd04a05c5dc
Joshua Alfonso <Joshua025icloud@gmail.com>
2025-07-20 20:26:03 +0800

    add first last page in pagination

--- src/ui/PaginationUI.tsx
+++ src/ui/PaginationUI.tsx
@@ -1,4 +1,5 @@
-import { PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
+import { Button } from "@/components/ui/button";
+// import { PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
 import { useSearchParams } from "react-router";
 
 
@@ -22,6 +23,18 @@ export const PaginationUI = ({ count, pageCount }: {count: number, pageCount: nu
          setSearchParams(searchParams);
     }
 
+    const FirstPage = () => {
+        const first = 1;
+        searchParams.set('page', String(first));
+        setSearchParams(searchParams);
+    }
+
+    const LastPage = () => {
+        const last = pageCount;
+         searchParams.set('page', String(last));
+        setSearchParams(searchParams);
+    }
+
     const start = (currentPage - 1) * PAGE_SIZE + 1;
     let end = currentPage * PAGE_SIZE;
     if (end > count) end = count;
@@ -31,24 +44,43 @@ export const PaginationUI = ({ count, pageCount }: {count: number, pageCount: nu
     if (pageCount <= 1) return null;
 
     return (
-        <div className="flex items-center justify-between mt-6 ">
+        <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-4 mt-6 ">
             <span className="text-xs font-medium"> {displayText} </span>
 
-            <div className="flex items-center gap-1">
+            <div className="flex items-center justify-center gap-1">
 
-                <PaginationPrevious
+                {/* <PaginationPrevious
                     onClick={prevPage}
                     className={currentPage === 1 ? 'pointer-events-none opacity-50' : 'cursor-pointer'}
                 />
 
-
+                <PaginationItem>
+                    <PaginationLink >{currentPage}</PaginationLink>
+                </PaginationItem>
 
                 <PaginationNext
                     onClick={nextPage}
                     className={
                         currentPage === pageCount ? 'pointer-events-none opacity-50' : 'cursor-pointer'
                     }
-                />
+                /> */}
+                <Button variant="ghost" onClick={FirstPage} disabled={currentPage <= 2}>
+                    <i className="fi fi-rr-angle-double-small-left grid place-items-center"></i>
+                    First
+                </Button>
+                <Button variant="ghost" onClick={prevPage}  disabled={currentPage === 1}>
+                    <i className="fi fi-rr-angle-small-left grid place-items-center"></i>
+                </Button>
+                <Button variant="ghost" className="pointer-events-none cursor-not-allowed">
+                    {currentPage}
+                </Button>
+                <Button variant="ghost" onClick={nextPage} disabled={currentPage === pageCount}>
+                    <i className="fi fi-rr-angle-small-right grid place-items-center"></i>
+                </Button>
+                <Button variant="ghost" onClick={LastPage} disabled={currentPage >= (pageCount - 1)}>
+                    Last
+                    <i className="fi fi-rr-angle-double-small-right grid place-items-center"></i>
+                </Button>
           
             </div>
             
