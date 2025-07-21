8caee0dfb0ef9af7e898c9b41224c6c3e2522c8d
Joshua Alfonso <Joshua025icloud@gmail.com>
2025-07-21 23:37:33 +0800

    add image to login

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -5,6 +5,9 @@ import { Button } from '@/components/ui/button';
 import { useLogin } from '@/features/auth/useLogin';
 import { ModeToggle } from '@/ui/ModeToggle';
 import { Loader2Icon, ShieldAlertIcon } from 'lucide-react';
+import { motion } from 'framer-motion';
+// import questions from '../assets/questions.png';
+import finance from '../assets/finance.png';
 
 export const LogIn = () => {
 
@@ -12,7 +15,7 @@ export const LogIn = () => {
 
     return (
         <>
-            <div className='min-h-screen w-full px-4'>
+            <div className='min-dvh w-full px-4'>
 
                 <header className='flex items-center justify-between max-w-7xl mx-auto h-[50px]'>
                     <div className='flex gap-2'>
@@ -26,22 +29,41 @@ export const LogIn = () => {
                 <section className='max-w-7xl mx-auto text-center mt-20'>
 
                     <div className='space-y-6'>
-                        <h1 className='text-3xl xl:text-4xl font-bold'>Take Control of Your <span className='text-[var(--color-primary)]'>Finances</span></h1>
-                        <p className='opacity-60 max-w-xl mx-auto text-sm md:text-base'>
+                        <motion.h1 
+                            className='text-3xl xl:text-4xl font-bold'
+                            initial={{ opacity: 0, y: 20 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 0.6, delay: 0.2 }}
+                        >
+                            Take Control of Your <span className='text-[var(--color-primary)]'>Finances</span>
+                        </motion.h1>
+                        <motion.p 
+                            className='opacity-60 max-w-xl mx-auto text-sm md:text-base'
+                            initial={{ opacity: 0, y: 20 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 0.6, delay: 0.3 }}
+                        >
                             Track expenses, set budgets, and achieve your financial goals with expense tracker app. 
                             Get insights that help you make smarter money decisions.
-                        </p>
-                        <Button 
-                            variant="secondary"
-                            onClick={() => login()}
-                            disabled={loading}
+                        </motion.p>
+                        
+                        <motion.div
+                            initial={{ opacity: 0, y: 20 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 0.6, delay: 0.4 }}
                         >
-                            { loading ? 
-                                <Loader2Icon className='animate-spin'/> : 
-                                <img src='google.png' width={18}/>
-                            }
-                                Sign in with Google 
-                        </Button>
+                            <Button 
+                                variant="secondary"
+                                onClick={() => login()}
+                                disabled={loading}
+                            >
+                                { loading ? 
+                                    <Loader2Icon className='animate-spin'/> : 
+                                    <img src='google.png' width={18}/>
+                                }
+                                    Sign in with Google 
+                            </Button>
+                        </motion.div>
                     </div>
 
                     {error && (
@@ -53,10 +75,15 @@ export const LogIn = () => {
                         </Alert>
                     )}
 
-
-                    {/* <div className='bg-[var(--color-primary)]/3  mt-20 p-4'>
-                        
-                    </div> */}
+                    {/* bg-[var(--color-primary)]/3 */}
+                    <motion.div 
+                        className='mt-5 px-4 flex justify-center items-center'
+                        initial={{ opacity: 0, y: 20 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.6, delay: 0.5 }}
+                    >
+                        <img src={finance} alt='logo' className='w-70 xl:w-100 '/>
+                    </motion.div>
 
                 </section>
 
